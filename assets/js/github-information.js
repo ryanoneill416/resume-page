1b93333bfdc294d72fc93b8ab15352c2ea0c95f0
Ryan O'Neill <ryanoneill416@gmail.com>
2022-08-07 20:24:43 +0000

    Render user information from github successfully

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,8 +1,29 @@
+function userInformationHTML(user) {
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href=${user.html_url} target="_blank">${user.login}</a>
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}">
+            </a>
+        </div>
+        <p>
+            Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}
+        </p>
+    </div>
+    `
+}
+
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h3>Please enter a GitHub username</h3>`);
         return;
     }
 
