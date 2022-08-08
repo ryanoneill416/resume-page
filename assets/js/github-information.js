9a6f596fdcebd5d447fb69c9a8ff070b9f5484f0
Ryan O'Neill <ryanoneill416@gmail.com>
2022-08-08 08:39:44 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -19,6 +19,26 @@ function userInformationHTML(user) {
 }
 
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repositories found :/</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+        <p><strong>Repository List:</strong></p>
+        <ul>
+            ${listItemsHTML.join("\n")}
+        </ul>
+    </div>`;
+}
+
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
